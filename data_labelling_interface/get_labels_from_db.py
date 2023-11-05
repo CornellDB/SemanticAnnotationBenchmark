32e657c05c1941d3d853577ec3f53d6b6b6a2457
sytan98 <st859@cornell.edu>
2023-11-05 15:32:59 -0500

    adding dirname in get_labels_from_db script

--- data_labelling_interface/get_labels_from_db.py
+++ data_labelling_interface/get_labels_from_db.py
@@ -1,17 +1,19 @@
 from datetime import datetime
+import os
 from dotenv import dotenv_values
 import pandas as pd
 import boto3
 
 from sqlalchemy import create_engine
 
-config = dotenv_values("../.env")
+dirname = os.path.dirname(__file__)
+config = dotenv_values(f"{dirname}/../.env")
 s3 = boto3.client(
     "s3", aws_access_key_id=config["AWS_ACCESS_KEY_ID"], aws_secret_access_key=config["AWS_SECRET_ACCESS_KEY"]
 )
 
 # Define the SQLite database connection string
-db_path = "semantic_annotation_backend.db"
+db_path = f"{dirname}/semantic_annotation_backend.db"
 engine = create_engine(f"sqlite:///{db_path}")
 
 # Write an SQL query to retrieve the data
@@ -22,5 +24,5 @@ df = pd.read_sql(sql_query, con=engine)
 
 # Now, you can work with the data in the DataFrame (df)
 csv_name = f"labels_{datetime.utcnow()}.csv"
-df.to_csv(csv_name)
-s3.upload_file(csv_name, "semantic-annotation-tables", f"labels/{csv_name}")
+df.to_csv(f"{dirname}/labels/{csv_name}")
+s3.upload_file(f"{dirname}/labels/{csv_name}", "semantic-annotation-tables", f"labels/{csv_name}")
